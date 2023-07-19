00d39015b7cff1c5e617dcdaee8622e280f4bc3e
Ultimateforce21 <--global>
2023-07-19 16:41:45 -0400

    Hello commented

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send("API is Running")
-})
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
 
-app.listen(5000, console.log("Server Started on PORT 5000"))
+app.listen(5000, console.log("Server Started on PORT 5000"));
+
+//Hello
