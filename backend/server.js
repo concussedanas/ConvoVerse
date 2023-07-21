df24a5414b40ae76e5e0f2b24b0e62875cfbce1a
Ultimateforce21 <--global>
2023-07-21 18:21:34 -0400

    Nothing major just testing commit

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,21 @@ const app = express();
 dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("API is Running Successfully");
+  res.send("API is Running Successfully");
 });
 
 app.get("/api/chat", (req, res) => {
-    res.send(chats);
+  res.send(chats);
 });
 
 app.get("/api/chat/:id", (req, res) => {
-    // console.log();
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
+  // console.log();
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, console.log('Server Started on PORT ' + [PORT]));
+app.listen(PORT, console.log("Server Started on PORT " + PORT));
 
 // testing updating through VS v3
