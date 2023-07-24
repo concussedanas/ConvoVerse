3b9114d13085cfc2ba9dd86e7fc1660a5fbb4ea1
Ultimateforce21 <--global>
2023-07-24 00:35:27 -0400

    Used axios to make http request from express local set server, notice axios relies on async calls

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const ChatPage = () => {
 
-  const fetchChats = () => {
-    
-  }
+  const [chats, setChats] = useState([]);
 
-  return <div>Chat Page</div>;
+  const fetchChats = async () => {
+    const {data} = await axios.get("/api/chat");
+     setChats(data);
+  };
+
+  useEffect(() => {
+      fetchChats();
+      // console.log("In use effect"); //used this to confirm this useEffect was working
+    }, [])
+
+  
+  return(
+    <div>
+      {chats.map((chat) => (
+      <div key = {chat._id}> {chat.chatName} </div>
+      ))}
+    </div>
+  );
+
 };
 
 export default ChatPage;
