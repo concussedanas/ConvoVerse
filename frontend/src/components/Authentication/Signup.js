93a63839dc7e0c3225b86aad3f75686befc95f2d
Ultimateforce21 <--global>
2023-07-31 01:23:05 -0400

    Started building postDetails function in Signup.js, however, it is not working as should. Continue to debug it.

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,87 +1,154 @@
-import React, {useState} from 'react'
-import { Input, InputGroup, InputRightElement, Button, FormControl, FormLabel, VStack } from '@chakra-ui/react'
+import React, { useState } from "react";
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  FormControl,
+  FormLabel,
+  VStack,
+} from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
 
 const Signup = () => {
-  const [show, setShow] = useState(false);  
+  const [show, setShow] = useState(false);
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [confirmpassword, setConfimpassword] = useState();
   const [password, setPassword] = useState();
   const [pic, setPic] = useState();
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   const handleClick = () => setShow(!show);
 
-  const postDetails = (pics) => {};
+  const postDetails = (pics) => {
+    setLoading(true);
+    if (pic === undefined) {
+      toast({
+        title: "Please select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "ConvoVerse");
+      data.append("cloud_name", "dqrlvyqdl");
+      fetch("https://api.cloudinary.com/v1_1/dqrlvyqdl/image/upload", {
+        method: 'post',
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+  };
 
   const submitHandler = () => {};
 
-  return <VStack>
-   
-    <FormControl id='first-name' isRequired>
+  return (
+    <VStack>
+      <FormControl id="first-name" isRequired>
         <FormLabel>Name</FormLabel>
         <Input
-            placeholder='Enter Your Name'
-            onChange={(e) => {setName(e.target.value)}}
+          placeholder="Enter Your Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
-    </FormControl>
-   
-    <FormControl id='email' isRequired>
+      </FormControl>
+
+      <FormControl id="email" isRequired>
         <FormLabel>Email</FormLabel>
         <Input
-            placeholder='Enter Your Email'
-            onChange={(e) => {setEmail(e.target.value)}}
+          placeholder="Enter Your Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
-    </FormControl>
+      </FormControl>
 
-    <FormControl id='password' isRequired>
+      <FormControl id="password" isRequired>
         <FormLabel>Password</FormLabel>
         <InputGroup>
-            <Input
-                type={show? "text" : "password"}
-                placeholder='Set Your Password'
-                onChange={(e) => {setPassword(e.target.value)}}
-            />
-            <InputRightElement width="4.5rem">
-                <Button h = "1.75rem" size = "sm" onClick={handleClick}>
-                    {show ? "Hide" : "Show"}
-                </Button>
-            
-            </InputRightElement>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Set Your Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
         </InputGroup>
-    </FormControl>
+      </FormControl>
 
-    <FormControl id='password' isRequired>
+      <FormControl id="password" isRequired>
         <FormLabel>Confirm Password</FormLabel>
         <InputGroup>
-            <Input
-                type={show? "text" : "password"}
-                placeholder='Confirm password'
-                onChange={(e) => {setConfimpassword(e.target.value)}}
-            />
-            <InputRightElement width="4.5rem">
-                <Button h = "1.75rem" size = "sm" onClick={handleClick}>
-                    {show ? "Hide" : "Show"}
-                </Button>
-            
-            </InputRightElement>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Confirm password"
+            onChange={(e) => {
+              setConfimpassword(e.target.value);
+            }}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
         </InputGroup>
-    </FormControl>
+      </FormControl>
 
-    <FormControl id='pic' isRequired>
+      <FormControl id="pic" isRequired>
         <FormLabel>Upload picture</FormLabel>
         <Input
-            type = "file"
-            p ={1.5}
-            accept="image/*"
-            onChange={(e) => {postDetails(e.target.files[0])}}
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e) => {
+            postDetails(e.target.files[0]);
+          }}
         />
-    </FormControl>
+      </FormControl>
 
-    <Button colorScheme='blue' width={"100%"} style={{marginTop: 15}} onClick={submitHandler}>
+      <Button
+        colorScheme="blue"
+        width={"100%"}
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={loading}
+      >
         Sign Up
-    </Button>
+      </Button>
+    </VStack>
+  );
+};
 
-  </VStack>
-}
-
-export default Signup
+export default Signup;
