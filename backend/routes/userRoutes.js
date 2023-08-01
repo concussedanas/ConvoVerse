ce88d4d627a3ff7ee9f53374ce1f691bf69674d9
Ultimateforce21 <--global>
2023-08-01 17:04:30 -0400

    Creating search query

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const { registerUser, authUser, allUsers} = require("../controllers/userControllers");
 
 const userRoutes = express.Router();
 
-userRoutes.route('/').post(registerUser);
+userRoutes.route("/").post(registerUser).get(allUsers);
 userRoutes.post('/login', authUser);
+
 
 
 module.exports = userRoutes;
