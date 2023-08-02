990d6a21b4c34aacccb0e62ef10b4f93f416c169
Ultimateforce21 <--global>
2023-08-02 14:03:16 -0400

    Created search api and created userAuth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { registerUser, authUser, allUsers} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const userRoutes = express.Router();
 
-userRoutes.route("/").post(registerUser).get(allUsers);
+userRoutes.route("/").post(registerUser).get(protect, allUsers);
 userRoutes.post('/login', authUser);
 
 
