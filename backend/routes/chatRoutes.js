2bf71ad742bafb39b657ddc0fe0e2b948b8ac726
Ultimateforce21 <--global>
2023-08-03 14:04:20 -0400

    Developed all the group endpoints

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const {accessChat} = require("../controllers/chatControllers");
+const {accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup} = require("../controllers/chatControllers");
 
 
 const chatRoutes = express.Router();
 
 chatRoutes.route("/").post(protect, accessChat);
-// chatRoutes.route("/").get(protect, fetchChats);
-// chatRoutes.route("/group").post(protect, createGroupChat);
-// chatRoutes.route("/rename").put(protect, renameGroup);
-// chatRoutes.route("/groupremove").put(protect, removeFromGroup);
-// chatRoutes.route("/groupadd").put(protect, addToGroup);
+chatRoutes.route("/").get(protect, fetchChats);
+chatRoutes.route("/group").post(protect, createGroupChat);
+chatRoutes.route("/rename").put(protect, renameGroup);
+chatRoutes.route("/groupadd").put(protect, addToGroup);
+chatRoutes.route("/groupremove").put(protect, removeFromGroup);
+
 
 
 module.exports = chatRoutes;
